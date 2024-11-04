d7f6e88c37eb3d54d25b12d8c46fd34fbba90097
Mohirjon013 <mohirjon26@gmail.com>
2024-11-04 08:36:25 +0100

    commit

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const SpotifyWebApi = require("spotify-web-api-node")
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.post("/login", (req, res) => {
+  const code = req.body.code
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:5173",
+    clientId: "d92688dabfb440ea808635e8804655b7",
+    clientSecret: "46dd57c6bc354e78913f1891578acd4a"
+  })
+  spotifyApi.authorizationCodeGrant(code).then(data => {
+    res.json({
+      accessToken: data.body.access_token,
+      refreshToken: data.body.refresh_token,
+      expiresIn: data.body.expires_in
+    })
+  }).catch(err => {
+    res.sendStatus(400)
+  })
+})
+
+app.listen(3001)
